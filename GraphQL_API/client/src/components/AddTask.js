fadc61e0e5ac509ba4bb787e90b7138c6f57b884
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-03-19 19:00:23 -0500

    Making queries from React

--- GraphQL_API/client/src/components/AddTask.js
+++ GraphQL_API/client/src/components/AddTask.js
@@ -1,7 +1,7 @@
-import {
-  useState,
-  //useEffect
-} from "react";
+import React, { useState } from 'react';
+import { graphql } from 'react-apollo';
+import { flowRight as compose } from 'lodash';
+import { getProjectsQuery, getTasksQuery, addTaskMutation } from '../queries/queries';
 
 
 function AddTask(props) {
@@ -11,7 +11,18 @@ function AddTask(props) {
     description: '',
     projectId: ''
   });
-
+  function displayProjects() {
+    var data = props.getProjectsQuery;
+    if (data) {
+      return <option disabled>Loading projects...</option>;
+    } else {
+      return data.projects.map(project => {
+        return (
+          <option key={project.id} value={project.id}>{project.title}</option>
+        );
+      });
+    }
+  }
 
   const handleChange = (e) => {
         const newInputs = {
@@ -22,76 +33,69 @@ function AddTask(props) {
         setInputs(newInputs)
   }
 
-  return ( <
-    form class = "task"
-    id = "add-task"
-    /*onSubmit = {...}*/ >
-    <
-    div className = "field" >
-    <
-    label > Task title: < /label> <
-    input type = "text"
-    name = "title"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.title
-    }
-    required /
-    >
-    < /
-    div > <
-    div className = "field" >
-    <
-    label > Weight: < /label> <
-    input type = "number"
-    name = "weight"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.weight
-    }
-    required /
-    >
-    < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > description: < /label> <
-    textarea name = "description"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.description
-    }
-    required /
-    >
-    < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > Project: < /label> <
-    select name = "projectId"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.projectId
-    }
-    required > < option value = ""
-    selected = "selected"
-    disabled = "disabled" > Select project < /option>  < /
-    select > < /
-    div >
-    <
-    button > + < /button> < /
-    form >
+  const submitForm = (e) => {
+    e.preventDefault();
+    props.addTaskMutation({
+      variables: {
+        title: inputs.title,
+        weight: inputs.weight,
+        description: inputs.description,
+        projectId: inputs.projectId
+      },
+      refetchQueries: [{ query: getTasksQuery }]
+    });
+  };
+
+
+  return (
+  <form className="task" id="add-task" onSubmit={submitForm}>
+    <div className="field">
+      <label>Task title:</label>
+      <input
+        type="text"
+        name="title"
+        onChange={handleChange}
+        value={inputs.title}
+        required
+      />
+    </div>
+    <div className="field">
+      <label>Weight:</label>
+      <input
+        type="number"
+        name="weight"
+        onChange={handleChange}
+        value={inputs.weight}
+        required
+      />
+    </div>
+    <div className="field" >
+      <label>description:</label>
+      <textarea
+        name="description"
+        onChange={handleChange}
+        value={inputs.description}
+        required
+      />
+    </div>
+    <div className="field">
+      <label>Project:</label>
+      <select
+        name="projectId"
+        onChange={handleChange}
+        value={inputs.projectId}
+        required
+      >
+        <option value = "" disabled>Select project</option>
+        {displayProjects()}
+      </select>
+    </div>
+    <button>+</button>
+    </form>
   );
 }
 
-export default AddTask;
+export default compose(
+  graphql(getProjectsQuery, { name: 'getProjectsQuery' }),
+  graphql(addTaskMutation, { name: 'addTaskMutation' })
+  )(AddTask);
--- GraphQL_API/client/src/components/TaskList.js
+++ GraphQL_API/client/src/components/TaskList.js
@@ -1,26 +1,37 @@
-import {
-  useState,
-  //useEffect
-} from "react";
+import React, { useState } from 'react';
+import { graphql } from 'react-apollo';
+import { getTasksQuery } from '../queries/queries';
+
 // components
 import TaskDetails from './TaskDetails';
+
 
 function TaskList(props) {
   const [state, setState] = useState({
     selected: null
   });
 
-  return ( <
-    div >
-    <
-    ul id = "task-list" > {
+  function displayTasks() {
+    var data = props.data;
+    if(data.loading) {
+      return <div>Loading tasks...</div>;
+    } else {
+      return data.tasks.map(task => {
+        return (
+          <li key={task.id} onClick={(e) => setState({ selected: task.id })}>
+            {task.title}
+          </li>
+        );
+      });
+    }
+  }
 
-    } <
-    /ul>  <
-    TaskDetails /
-    > < /
-    div >
+  return (
+    <div>
+      <ul id="task-list">{displayTasks()}</ul>
+      <TaskDetails taskId={state.selected} />
+    </div>
   );
 }
 
-export default TaskList;
+export default graphql(getTasksQuery)(TaskList);
